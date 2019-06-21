7d1a76292db8d0eaaf6deded2275bbf091f78847
Kim Usi <kusi@bigvikinggames.com>
2019-06-21 18:35:03 -0400

    Add shooting assets

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,17 +10,12 @@ public class PlayerMovement : MonoBehaviour
     float verticalMove = 0f;
     float moveSpeed = 10f;
 
-    Vector2 mousePos;
-
     public Vector2 mousePointWorld;
 
     void Update()
     {
         horizontalMove = Input.GetAxis("Horizontal") * moveSpeed;
         verticalMove = Input.GetAxis("Vertical") * moveSpeed;
-
-        mousePos.x = Input.mousePosition.x;
-        mousePos.y = Input.mousePosition.y;
     }
     
     void FixedUpdate()
@@ -30,11 +25,8 @@ public class PlayerMovement : MonoBehaviour
         rigidBody.MovePosition(rigidBody.position + movementVector);
 
         // Rotate the player based on their mouse position
-        mousePointWorld = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, Camera.main.nearClipPlane));
-        // Convert mouse position from world to local space. Normalize it
-        Vector3 mousePointLocal = transform.InverseTransformPoint(mousePointWorld);
-        mousePointLocal.z = 0;
-        transform.Rotate(0,0,Vector3.Angle(transform.forward, mousePointLocal.normalized));
-
+        mousePointWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = new Vector2(mousePointWorld.x - transform.position.x, mousePointWorld.y - transform.position.y);
+        transform.up = direction;
     }
 }
