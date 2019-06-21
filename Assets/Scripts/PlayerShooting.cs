3e7184f3765c921049abfb261b97a47f9706103b
Kim Usi <kusi@bigvikinggames.com>
2019-06-21 19:31:59 -0400

    Link idle with shooting animation

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // When the LMB is pressed
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            // trigger the shoot animation
+            anim.SetTrigger("shoot");
+
+            // Spawn a bullet
+        }
+    }
+}
