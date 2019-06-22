78c23f2cc06704869a22c5d7887a9a68af03c488
Kim Usi <kusi@bigvikinggames.com>
2019-06-22 17:08:03 -0400

    randomize walls, shrink enemy

---
+++ Assets/Scripts/RandomWall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWall : MonoBehaviour
+{
+
+    public List<Sprite> walls = new List<Sprite>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int wallNumber = Random.Range(0, 5);
+        SpriteRenderer spriteRen = gameObject.GetComponent<SpriteRenderer>();
+        spriteRen.sprite = walls[wallNumber];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
