c65c0e6b65828d4cd0723faf3970264ec4c14365
Kim Usi <kusi@bigvikinggames.com>
2019-06-22 17:43:51 -0400

    fix enemy hitbox again, add moving object

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    public Vector3 startPoint;
+    public Vector3 endPoint;
+    public float speed = 1.0f;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(startPoint, endPoint, (Mathf.Sin(Time.time * speed ) + 1.0f) / 2);
+    }
+}
