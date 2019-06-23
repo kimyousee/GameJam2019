04a88f39a39cff2a37b2ff804fb8e37613d13d8d
Kim Usi <kusi@bigvikinggames.com>
2019-06-22 20:55:41 -0400

    Add sounds

---
+++ Assets/Scripts/MusicClass.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * A singleton to manage the background music
+ */
+public class MusicClass : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private static MusicClass instance = null;
+    public static MusicClass Instance
+    {
+        get { return instance; }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+}
---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private void Awake()
+    {
+
+        _audioSource = GetComponent<AudioSource>();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource.time = _audioSource.clip.length * 0.45f;
+        _audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
